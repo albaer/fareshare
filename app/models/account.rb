44a4ede18224b1f9308b1d37981dbfa9e58609b2
Anne Baer <albaer@gmail.com>
2014-07-02 17:51:23 -0400

    basic associations working

--- app/models/account.rb
+++ app/models/account.rb
@@ -2,6 +2,6 @@ class Account < ActiveRecord::Base
 	belongs_to :group
 	belongs_to :user
 	has_many :created_expenses, foreign_key: :creator_id, class_name: "Expense"
-	has_many :charges, foreign_key: :contributor_id
-	has_many :contributing_expenses, through: :charges
+	has_many :charges
+	has_many :expenses, through: :charges
 end
